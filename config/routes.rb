5367d6127fb8afb4cfabd0cc3d923e11ba674729
Edicto Cortez <emanuelcortezr@gmail.com>
2020-03-26 17:44:44 -0300

    Se añadio una tercera página

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: "pages#one"
   get 'pages/one'
   get 'pages/two'
+  get 'pages/three'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
